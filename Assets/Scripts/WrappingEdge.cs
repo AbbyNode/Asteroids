86b69bbaed97f8396373249c689fa794d5dfefe9
Abby <AbbyNode@users.noreply.github.com>
2017-07-12 00:33:06 -0400

    Completed movement and wrapping

---
+++ Assets/Scripts/WrappingEdge.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WrappingEdge : MonoBehaviour {
+	public bool wrapOnX;
+
+	private void OnTriggerEnter2D(Collider2D collision) {
+		if (collision.CompareTag("Player")) {
+			float xPos = collision.transform.position.x;
+			float yPos = collision.transform.position.y;
+			if (wrapOnX) {
+				xPos = xPos * -0.9f;
+			} else {
+				yPos = yPos * -0.9f;
+			}
+			collision.transform.position = new Vector2(xPos, yPos);
+		}
+	}
+}
